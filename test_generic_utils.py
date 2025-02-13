2ae581d6a9764dc6d058e55d0670f3c644f552c1
fanni.jako <fanni.jako@lhsystems.com>
2025-02-13 12:51:25 +0100

    test positivenumber and stringvalue

--- test_generic_utils.py
+++ test_generic_utils.py
@@ -1,35 +1,38 @@
-import pytest
+import pytest # type: ignore
 from  generic_utils import *
 
 class TestClass:
     number = PositiveNumber()
+    string = StringValue()
 
 @pytest.fixture
 def instance():
     return TestClass()
 
-def test_positive_number_assignment(instance):
+def test_positive_number_assignment_to_integer(instance):
     instance.number = 10
     assert instance.number == 10
 
-def test_negative_number_assignment(instance):
+def test_negative_number_assignment_to_integer(instance):
     with pytest.raises(ValueError, match="positive number expected"):
         instance.number = -5
 
-def test_zero_assignment(instance):
+def test_zero_assignment_to_integer(instance):
     with pytest.raises(ValueError, match="positive number expected"):
         instance.number = 0
 
-def test_non_numeric_assignment(instance):
+def test_non_numeric_assignment_to_integer(instance):
     with pytest.raises(ValueError, match="positive number expected"):
         instance.number = "string"
 
-class TestClass:
-    string = StringValue()
-
-@pytest.fixture
-def instance():
-    return StringValue()
-
-def test_string_assignment(instance):
+def test_string_assignment_to_string(instance):
     instance.string = 'string'
+    assert instance.string == 'string'
+
+def test_integer_assignment_to_string(instance):
+    with pytest.raises(ValueError, match="string expected"):
+        instance.string = 10
+
+def test_none_assignment_to_string(instance):
+     with pytest.raises(ValueError, match="string expected"):
+        instance.string = None
