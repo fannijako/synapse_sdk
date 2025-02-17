8a888895b072b0532b737274117d9c175b272e52
fanni.jako <fanni.jako@lhsystems.com>
2025-02-17 12:42:25 +0100

    automatically commited by commit.sh

--- commit.sh
+++ commit.sh
@@ -80,6 +80,8 @@ done
 
 echo "Generated notebook files have been added to the repository. Starting the commit..."
 
+# TODO: commit to TISC and run integration tests
+
 cd $REPOSITORY_NAME
 git add .
 git commit -m $COMMIT_MESSAGE
