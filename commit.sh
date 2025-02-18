37806a94130f55e5fb887f70f13dc623176d0028
fanni.jako <fanni.jako@lhsystems.com>
2025-02-18 13:10:47 +0100

    linter

--- commit.sh
+++ commit.sh
@@ -34,6 +34,7 @@ pytest test_utils.py || exit 1
 
 echo "All tests passed. Linter started ..."
 
+pylint --disable=W1203,W0603,W0718,E0401,C0114,C0116,W0201,W0621,W0511,C0301 $(git ls-files '*.py')
 
 echo "Linter passed. Deactivating virtual environment..."
 
